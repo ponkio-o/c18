5b5b0e7202c2e744555ba2209cbc4b8d05f9f986
ponkio-o <pasokon218@gmail.com>
2018-06-21 03:27:52 +0900

    first commit

---
+++ m102.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define XLEN 13
+#define YLEN 13
+int main(){
+    int i,j;
+    int a[XLEN][YLEN];
+    int yokomax[XLEN];
+    int tatemax[YLEN];
+
+    //初期値のセット
+    for(j=0;j<YLEN;j++){
+        for(i=0;i<XLEN;i++){
+            a[j][i]=rand()%100;
+        }
+    }
+
+    //横の最大値計算
+    for(j=0;j<YLEN;j++){
+        yokomax[j]=a[j][0];
+        for(i=0;i<XLEN;i++){
+            if(yokomax[j]<a[j][i]){
+                yokomax[j]=a[j][i];
+            }
+        }
+    }
+
+    //縦の最大値計算
+    for(j=0;j<YLEN;j++){
+        tatemax[j]=a[j][0];
+        for(i=0;i<XLEN;i++){
+            if(tatemax[j]<a[i][j]){
+                tatemax[j]=a[i][j];
+            }
+        }
+    }
+
+    //横軸の最大値の出力
+    for(j=0;j<YLEN;j++){
+        for(i=0;i<XLEN;i++){
+            printf("%2d ",a[j][i]);
+        }
+        printf(" | %d",yokomax[j]);
+        printf("\n");
+    }
+
+    //区切り線
+    for(i=0;i<XLEN;i++){
+        printf("---");
+    }
+    printf("\n");
+
+    //縦軸の最大値の出力
+    for(i=0;i<YLEN;i++){
+        printf("%2d ",tatemax[i]);
+    }
+    printf("\n");
+}
